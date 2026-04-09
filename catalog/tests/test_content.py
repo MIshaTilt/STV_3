8e5cfea842980af5394c4cc89d5708b2af393c3b
MIshaTilt <mdsv@inbox.ru>
2026-04-09 12:07:18 +0300

    update tests for practice update

--- catalog/tests/test_content.py
+++ catalog/tests/test_content.py
@@ -1,60 +1,90 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.db.models.query import QuerySet
 from catalog.models import Table, TableMaterial
 
 class ContentTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.mat1 = TableMaterial.objects.create(name="Дерево")
-        cls.mat2 = TableMaterial.objects.create(name="Стекло")
+        # Создаем два материала
+        cls.mat_wood = TableMaterial.objects.create(name="Дерево")
+        cls.mat_glass = TableMaterial.objects.create(name="Стекло")
         
-        cls.table1 = Table.objects.create(brand="A_Wood", length=100, width=50, weight=20, material=cls.mat1)
-        cls.table2 = Table.objects.create(brand="B_Glass", length=90, width=90, weight=None, material=cls.mat2)
+        # Создаем столы с РАЗНЫМИ параметрами для проверки сортировки
+        # Вес (weight) - необязательное поле. Длина (length) - обязательное.
+        cls.table_a = Table.objects.create(brand="A_Wood_Table", length=150.0, width=80.0, weight=25.0, material=cls.mat_wood)
+        cls.table_b = Table.objects.create(brand="B_Glass_Table", length=90.0, width=90.0, weight=None, material=cls.mat_glass)
+        cls.table_c = Table.objects.create(brand="C_Wood_Mini", length=50.0, width=50.0, weight=5.0, material=cls.mat_wood)
 
     # TC-02: Список содержит объекты
-    def test_list_context_not_empty(self):
+    def test_list_context_objects_not_empty(self):
         response = self.client.get(reverse('catalog:table_list'))
         self.assertIn('tables', response.context)
-        self.assertTrue(len(response.context['tables']) > 0)
+        # Проверяем, что передается именно QuerySet и он не пуст
+        self.assertIsInstance(response.context['tables'], QuerySet)
+        self.assertEqual(len(response.context['tables']), 3)
 
     # TC-03: Фильтрация по типу
-    def test_list_filter_by_type_success(self):
-        # Передаем ?type=<id_дерева>
-        response = self.client.get(reverse('catalog:table_list'), {'type': self.mat1.pk})
-        self.assertEqual(len(response.context['tables']), 1)
-        self.assertEqual(response.context['tables'][0].material, self.mat1)
-
-    # Проверка сортировки (доп.)
-    def test_list_sort_by_optional_field(self):
-        # Сортируем по весу (weight - необязательное поле)
-        response = self.client.get(reverse('catalog:table_list'), {'sort': 'weight'})
-        self.assertEqual(response.status_code, 200)
+    def test_list_filter_by_type_expected(self):
+        url = reverse('catalog:table_list')
+        # Передаем query-параметр ?type=ID_Дерева
+        response = self.client.get(url, {'type': self.mat_wood.pk})
         tables = response.context['tables']
-        # Проверяем, что сортировка отработала (None обычно идут первыми или последними в БД)
-        self.assertTrue(len(tables) == 2)
+        
+        self.assertEqual(len(tables), 2) # У нас 2 деревянных стола
+        for table in tables:
+            self.assertEqual(table.material, self.mat_wood)
 
-    # TC-04: Детальная страница содержит контекст
-    def test_detail_context_has_object(self):
-        response = self.client.get(reverse('catalog:table_detail', args=[self.table1.pk]))
+    # TC-04 (контекст): Детальная страница содержит нужный объект
+    def test_detail_context_contains_object(self):
+        url = reverse('catalog:table_detail', args=[self.table_a.pk])
+        response = self.client.get(url)
         self.assertIn('table', response.context)
-        self.assertEqual(response.context['table'], self.table1)
+        self.assertEqual(response.context['table'].brand, "A_Wood_Table")
 
     # TC-07: About содержит текст
-    def test_about_contains_text(self):
+    def test_about_contains_text_expected(self):
         response = self.client.get(reverse('catalog:about'))
-        # Текст должен совпадать с тем, что у тебя в about.html
-        self.assertContains(response, "О проекте") 
+        self.assertContains(response, "О проекте", status_code=200)
 
-    # TC-08: Пустой список товаров
-    def test_list_empty_is_correct(self):
-        Table.objects.all().delete() # Удаляем все столы
+    # TC-08: Пустой список товаров (создаем пустую БД прямо в тесте)
+    def test_list_empty_expected(self):
+        Table.objects.all().delete() # Очищаем столы
         response = self.client.get(reverse('catalog:table_list'))
         self.assertEqual(len(response.context['tables']), 0)
-        # Проверяем, что шаблон корректно отрендерил пустой список
         self.assertContains(response, "Товаров пока нет")
 
     # TC-10: Контекст имеет нужные ключи
     def test_list_context_keys_exist(self):
         response = self.client.get(reverse('catalog:table_list'))
         self.assertIn('tables', response.context)
-        self.assertIn('types', response.context)  # Проверяем наличие 'types' из get_context_data
+        self.assertIn('types', response.context)
+        self.assertIsInstance(response.context['types'], QuerySet)
+
+    # ДОП. ТЕСТ: Сортировка по умолчанию (по бренду)
+    def test_list_sort_default_by_brand(self):
+        response = self.client.get(reverse('catalog:table_list'))
+        tables = list(response.context['tables'])
+        # Ожидаем порядок A, B, C
+        self.assertEqual(tables[0], self.table_a)
+        self.assertEqual(tables[2], self.table_c)
+
+    # ДОП. ТЕСТ: Сортировка по обязательному атрибуту (Длина)
+    def test_list_sort_by_required_attr_length(self):
+        # ?sort=length
+        response = self.client.get(reverse('catalog:table_list'), {'sort': 'length'})
+        tables = list(response.context['tables'])
+        # Ожидаем: 50 (table_c), 90 (table_b), 150 (table_a)
+        self.assertEqual(tables[0], self.table_c)
+        self.assertEqual(tables[1], self.table_b)
+        self.assertEqual(tables[2], self.table_a)
+
+    # ДОП. ТЕСТ: Сортировка по НЕобязательному атрибуту (Вес)
+    def test_list_sort_by_optional_attr_weight(self):
+        # ?sort=weight
+        response = self.client.get(reverse('catalog:table_list'), {'sort': 'weight'})
+        tables = list(response.context['tables'])
+        # Ожидаем, что NULL (table_b) будет либо в начале, либо в конце (зависит от БД SQLite).
+        # Проверим, что 5.0 (table_c) идет точно перед 25.0 (table_a)
+        weight_list = [t.weight for t in tables if t.weight is not None]
+        self.assertEqual(weight_list, [5.0, 25.0])
--- catalog/tests/test_routes.py
+++ catalog/tests/test_routes.py
@@ -5,37 +5,42 @@ from catalog.models import Table, TableMaterial
 class RoutesTest(TestCase):
     @classmethod
     def setUpTestData(cls):
+        # Подготовка данных для тестов маршрутов
         cls.material = TableMaterial.objects.create(name="Дерево")
-        cls.table = Table.objects.create(brand="TestRouteTable", length=100, width=50, material=cls.material)
+        cls.table = Table.objects.create(
+            brand="RouteTestBrand", length=100.0, width=50.0, material=cls.material
+        )
 
     # TC-01: Главная страница каталога
-    def test_list_status_200(self):
+    def test_list_status_200_expected(self):
         url = reverse('catalog:table_list')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'catalog/table_list.html')
 
-    # TC-04 (часть): Детальная страница - доступность
-    def test_detail_status_200(self):
+    # TC-04 (маршрут): Детальная страница
+    def test_detail_status_200_expected(self):
         url = reverse('catalog:table_detail', args=[self.table.pk])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'catalog/table_detail.html')
 
     # TC-05: Несуществующий товар
-    def test_detail_999_404(self):
-        url = reverse('catalog:table_detail', args=[999])
+    def test_detail_404_if_not_exists(self):
+        url = reverse('catalog:table_detail', args=[9999])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
 
     # TC-06: Страница About
-    def test_about_status_200(self):
+    def test_about_status_200_expected(self):
         url = reverse('catalog:about')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'catalog/about.html')
 
     # TC-09: URL через reverse()
-    def test_list_reverse_matches_url(self):
+    def test_list_reverse_match_url(self):
         url = reverse('catalog:table_list')
-        # Если в urls.py у тебя path('', TableListView...), то url будет '/'
+        # В зависимости от твоего config/urls.py, путь может быть '/' или '/products/'
+        # Замени '/' на то, что у тебя реально используется
         self.assertEqual(url, '/')
